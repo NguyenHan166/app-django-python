df7eec6ab35c4110325d8304fbc2dfbacd6c8f82
NguyenHan <nvhan166@gmail.com>
2023-10-14 02:01:52 +0700

    update file

--- item/views.py
+++ item/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render,get_object_or_404
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.views.decorators.cache import never_cache
 
 from .models import Category, Item , Cart
 from .forms import EditItemForm, NewItemForm
@@ -15,24 +16,36 @@ def items(request):
     items = Item.objects.filter(is_sold=False)
     category_id = request.GET.get('category' , 0)
     sort_by = request.GET.get('sort_by','')
+    from_price = request.GET.get('from_price', '')
+    to_price = request.GET.get('to_price' , '')
+    if from_price == '':
+        from_price = 0
+    if to_price == '':
+        to_price = 100000000000
 
     if category_id != 0:
         items = items.filter(category_id=category_id)
 
     if query:
         items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    
+    items = items.filter(price__gte=from_price , price__lte=to_price)
 
     if (sort_by == 'asc'):
         items = sorted(items , key=lambda item: item.price)
     else:
         items = sorted(items, key=lambda item: item.price , reverse=True)
+    
+    
 
     return render(request, 'item/items.html', {
         'items': items,
         'query': query,
         'categories': categories,
         'category_id': int(category_id),
-        'sort_by' :sort_by
+        'sort_by' :sort_by,
+        'from_price': from_price,
+        'to_price': to_price,
     })
 
 def detail(request, pk):
@@ -91,7 +104,7 @@ def cart(request):
     cart = Cart.objects.filter(user = request.user)[0]
     items = cart.items.all()
     total_price = sum([x.price for x in items])
-    stt = range(1 , len(items) + 2)
+    stt = range(1 , len(items) + 1)
     items = zip(items, stt)
     return render(request, 'item/cart.html' , {
         'cart' : cart,
@@ -102,6 +115,7 @@ def cart(request):
 @login_required
 def addCart(request , pk):
     item = Item.objects.get(pk=pk)
+    page = request.GET.get('page' ,'')
     print(request.user)
     cart = Cart.objects.filter(user=request.user)
     if not cart.exists():
@@ -110,17 +124,24 @@ def addCart(request , pk):
     cart = Cart.objects.filter(user=request.user)[0]
     cart.items.add(item)
     cart.save()
+    if page != '':
+        return redirect("/items/" + str(pk))
     return redirect('/')
 
 @login_required
+@never_cache
 def removecart(request,pk):
     try:
         cart = Cart.objects.filter(user = request.user)[0]
-        item = cart.items.all()[pk-1]
+        item = cart.items.all()[pk - 1]
         print(item)
+        total_price = sum([x.price for x in cart.items.all()])
+        print(total_price)
+        total_price -= item.price
         cart.items.remove(item)
         return JsonResponse({
             'status' : 'success',
+            'total_price' : total_price
         })
     except Exception as e:
         return JsonResponse({'status': 'error', 'message': str(e)})
